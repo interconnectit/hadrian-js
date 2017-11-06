e1f0d7ecd518bd81c37d35406c7cde3910e0c297
Evgenii Nasyrov <inasyrov@yandex.ru>
2017-11-06 14:17:59 +0000

    Add initial api implementation

---
+++ src/api/index.js
@@ -0,0 +1,26 @@
+/*!
+ * hadrian
+ * Copyright(c) 2017 interconnect/it
+ * MIT Licensed
+ */
+
+'use strict'
+
+const axios = require('axios')
+const cookie = require('js-cookie')
+const config = require('../api').api
+
+const metrics = require('./metrics')
+
+const instance = axios.create({
+    baseURL: config.baseUrl,
+    timeout: config.timeout
+})
+
+// TODO: interact with a storage
+instance.interceptors.request.use((config) => config)
+
+// TODO: interact with a storage
+instance.interceptors.response.use((response) => response)
+
+exports.metrics = metrics(instance)
