9e525f6bd28cd8811b814d1f8bffd9fec7fff15c
Evgenii Nasyrov <inasyrov@yandex.ru>
2017-11-07 15:21:16 +0000

    WIP bootstrap implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 'use strict'
 
+const _merge = require('lodash').merge
+const Hadrian = require('./core/hadrian')
+const defaults = require('./defaults')
+
 var hadrian = {}
+
+// Expose Hadrian class
+hadrian.Hadrian = Hadrian
+
+// Factory for creating new instances
+hadrian.create = function create (config) {
+    return new Hadrian(_merge(defaults, config))
+}
 
 module.exports = hadrian
