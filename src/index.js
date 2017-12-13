59b3c6da544683cba01391b7587c6c7855444712
Andrea <andrea@interconnectit.com>
2017-12-13 11:36:55 +0000

    Add additional axios options

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import Hadrian from './core/hadrian'
  * Create a new hadrian instance
  *
  * @param {String} siteUuid
+ * @param {String} axiosOptions
  * @return {Hadrian}
  */
-function factory (siteUuid) {
-    return new Hadrian(siteUuid)
+function factory (siteUuid, axiosOptions = {}) {
+    return new Hadrian(siteUuid, axiosOptions)
 }
 
 module.exports = factory
