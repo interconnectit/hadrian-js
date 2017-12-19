8260e1a3a0967e857c1606fff1988f1a9068cd2a
Andrea <andrea@interconnectit.com>
2017-12-19 15:08:36 +0000

    case 29878. add couple more tests and fix some glitches. (Need to debug and fix the test for the callback on evaluation's response)

--- tests/core/hadrian.test.js
+++ tests/core/hadrian.test.js
@@ -1,68 +1,89 @@
 import Hadrian from '../../src/core/hadrian'
 import axios from 'axios'
-import jsCookie from 'js-cookie'
 
 describe('Hadrian', () => {
     describe('constructor', () => {
         it('should setup site uuid parameter', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
-            expect(instance.siteUuid).toBe('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            expect(hadrian.siteUuid).toBe('63a67339-f3c9-47eb-8551-fc7c28bab06c')
         })
 
-/*        it('should setup axios instance with default parameters', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c', {})
-            console.log(instance.axios.interceptors.InterceptorManager.handlers)
-            console.log(axios.create())
-            expect(instance.axios).toEqual(axios.create({baseURL: 'https://api.hadrianpaywall.com', timeout: 2000}))
-        })*/
+        it('should setup default baseURL for axios', () => {
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            expect(hadrian.axios.defaults.baseURL).toBe('https://api.hadrianpaywall.com')
+        })
 
         it('should setup custom baseURL for axios', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c', {baseURL: 'https://hadrian-api-staging.herokuapp.com/metrics'})
-            expect(instance.axios.defaults.baseURL).toBe('https://hadrian-api-staging.herokuapp.com/metrics')
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c', {baseURL: 'https://hadrian-api-staging.herokuapp.com/metrics'})
+            expect(hadrian.axios.defaults.baseURL).toBe('https://hadrian-api-staging.herokuapp.com/metrics')
         })
 
         /* ToDo */
-        /*it('should setup the sessionUuid', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
-            expect(instance.sessionUuid).toBe() // PROBABLY NEED TO MOCK THE JSCOOKIE
+/*        it('should setup the sessionUuid', () => {
+            const jsCookie = {}
+            jsCookie.get = jest.fn()
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            expect(jsCookie.get).toHaveBeenCalledWith('hadrian-session-uuid')
         })*/
 
         /* ToDo */
         /*it('should setup the subscriberUuid', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
-            expect(instance.subscriberUuid).toBe() // PROBABLY NEED TO MOCK THE JSCOOKIE
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            expect(hadrian.subscriberUuid).toBe() // PROBABLY NEED TO MOCK THE JSCOOKIE
         })*/
     })
 
     describe('on conditions', () => {
-        it('should add triggers', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+        it('should add triggers with callbacks', () => {
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
             const callback = jest.fn()
-            instance.on({quota: {views: 3, left: 2}}, callback)
-            expect(instance.triggers).toContainEqual({ condition: {quota: {views: 3, left: 2}}, callback: callback })
+            hadrian.on({quota: {views: 3, left: 2}}, callback)
+
+            expect(hadrian.triggers).toContainEqual({condition: {quota: {views: 3, left: 2}}, callback: callback})
         })
     })
 
     describe('evaluation', () => {
         /* ToDo */
-/*        it('should evaluate correctly', () => {
-            const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c', {baseURL: 'https://hadrian-api-staging.herokuapp.com/metrics'})
-            const payload = '{"post_id":7342,"post_parent_id":0,"post_author_id":33,"post_type":"post","post_slug":"gender-champions-case-study","post_status":"publish","post_created_date":"2017-04-10T13:23:49+00:00","post_updated_date":"2017-05-10T10:18:47+00:00","post_terms":[{"term_id":4,"term_parent_id":0,"term_taxonomy":"category","term_slug":"portfolio"}]}'
+        it('should create post request to metrics', () => {
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+
+            const axios = {}
+            const response = {data: {data: {trigger: {quota: {views: 3, left: 2}}}}}
+            axios.post = jest.fn(() => Promise.resolve(response))
+            hadrian.axios = axios
+
+            const payload = {post_id: 1}
+
+            hadrian.evaluate(payload)
+
+            expect(axios.post).toHaveBeenCalledWith('metrics', {payload})
+        })
+/* ToDo: this should work but for some reason tells that the mock-callback is not called even though it is definitely there */
+/*        it('should evaluate the given response and execute the callback', () => {
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+
+            const axios = {}
+            const response = {data: {data: {trigger: {quota: {views: 3, left: 2}}}}}
+            axios.post = jest.fn(() => Promise.resolve(response))
+            hadrian.axios = axios
+
             const callback = jest.fn()
-            instance.on({quota: {views: 3, left: 2}}, callback)
-            instance.evaluate(payload)
-            expect(callback).toHaveBeenCalled() // MAYBE I NEED TO CHECK A PROMISE?
+            hadrian.on(response.data.data.trigger, callback)
+
+            const payload = {post_id: 1}
+
+            hadrian.evaluate(payload)
+
+            expect(callback).toHaveBeenCalledWith(response.data.data)
         })*/
     })
 
-/*    describe('evaluateMetricsResponse', () => {
-        const instance = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c', {baseURL: 'https://hadrian-api-staging.herokuapp.com/metrics'})
-        const trigger = {quota: {views: 3, left: 0}}
-        const data = {data: {trigger: trigger}}
-        const callback = function (data) { return data }
-        instance.on(trigger, callback)
-        it('should compare correctly', () => {
-            expect(evaluateMetricsResponse.call(instance)).toBe()
+/*    describe('axios', () => {
+        it('should add site header to axios request', () => {
+            const hadrian = new Hadrian('63a67339-f3c9-47eb-8551-fc7c28bab06c')
+            console.log(hadrian.axios.interceptors.request)
+            expect(hadrian.axios).toBe()
         })
     })*/
 })
