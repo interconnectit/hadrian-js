66b7189224e9aa01ff34a7e1ce4e5be3a2a3369c
Evgenii Nasyrov <inasyrov@yandex.ru>
2018-01-19 11:01:50 +0000

    Adjust the tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,7 +2,7 @@ import factory from '../src/index'
 import Hadrian from '../src/core/hadrian'
 
 describe('factory', () => {
-    it('return new Hadrian instance', () => {
+    it('returns new Hadrian instance', () => {
         expect(factory()).toBeInstanceOf(Hadrian)
     })
 })
